6e60a1c960abf46c66c790f235a7919e3195be4d
Anna Brooksby <johnhbrooksby@yahoo.com>
2023-04-18 15:59:59 -0600

    Adds pokedex to DOM

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 import Header from './components/Header.jsx';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import TeamScreen from './screens/TeamScreen';
 import DexScreen from './screens/DexScreen';
 
--- src/screens/DexScreen.jsx
+++ src/screens/DexScreen.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const DexScreen = () => {
+    const [pokemon, setPokemon] = useState([])
+
+    useEffect(() => {
+        axios.get(`https://pokeapi.co/api/v2/pokemon?limit=1000`).then(res => {
+            setPokemon(res.data.results)
+        })
+    }, [])
+
+    const pokemonDisplay = pokemon.map((poke, index) => {
+        return <h4>{poke.name}</h4>
+    })
 
     return (
         <div>
             <h1>Pokedex</h1>
+            {pokemonDisplay}
         </div>
     )
 }
